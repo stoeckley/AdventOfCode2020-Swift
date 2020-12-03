f97f1c05a06df0f8494b997bafd93960b6f3a2b3
Andrew Stoeckley <andrewjstoeckley@gmail.com>
2020-12-03 06:44:19 +0100

    day three

--- AdventCode2020/3.swift
+++ AdventCode2020/3.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+func solve3(_ input: String) -> Int {
+    let lines = readLines(input).map { Array($0) }
+    var x = 0
+    var trees = 0
+    for y in 0..<lines.count {
+        if lines[y][x % lines.first!.count] == "#" {
+            trees += 1
+        }
+        x += 3
+    }
+    return trees
+}
+
+// part 2
+
+func travel(_ input: String, _ xStride: Int, _ yStride: Int) -> Int {
+    let lines = readLines(input).map { Array($0) }
+    var x = 0
+    var trees = 0
+    for y in stride(from: 0, to: lines.count, by: yStride) {
+        if lines[y][x % lines.first!.count] == "#" {
+            trees += 1
+        }
+        x += xStride
+    }
+    return trees
+}
+
+func solve3b(_ input: String, _ strides: [(x: Int, y: Int)]) -> Int {
+    return strides.reduce(1) { $0 * travel(input, $1.x, $1.y) }
+}
--- AdventCode2020/main.swift
+++ AdventCode2020/main.swift
@@ -16,3 +16,7 @@ import Foundation
 //print(solve2b(input2))
 //print(alternate2(input2))
 //print(alternate2b(input2))
+
+// Day 3
+print(solve3(input3))
+print(solve3b(input3, [(1,1),(3,1),(5,1),(7,1),(1,2)])) // 3510149120
